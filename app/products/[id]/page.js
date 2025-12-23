67f0ddfaef78af745deedc40a0904730332438a7
Raje0088 <rajchincholkar58@gmail.com>
2025-12-23 23:32:04 +0530

    Data Fetching using API

--- app/products/[id]/page.js
+++ app/products/[id]/page.js
@@ -1,7 +1,39 @@
-export default async function content({params}){
-     const { id } = await params;
-    console.log("parmas", id)
+async function getSingleProduct(id) {
+    const res = await fetch(`https://dummyjson.com/products/${id}`, {
+        next: { revalidate: 3600 }
+    });
+
+    if (!res.ok) return null;
+    return res.json();
+}
+
+export default async function content({ params }) {
+    const { id } = await params;
+    const product = await getSingleProduct(id);
     return (
-        <h1> product {id} details page - Content coming soon</h1>
+        <div className="p-8 border rounded-lg max-w-2xl mx-auto mt-10 shadow-lg">
+            <h1 className="text-3xl font-bold mb-4">{product.title}</h1>
+
+            <div className="flex flex-col md:flex-row gap-6">
+                <img
+                    src={product.thumbnail}
+                    alt={product.title}
+                    className="w-full md:w-1/2 rounded bg-gray-100"
+                />
+                <div>
+                    <p className="text-gray-500 mb-2 italic">{product.category}</p>
+                    <p className="text-xl font-semibold text-green-600 mb-4">
+                        Price: ${product.price}
+                    </p>
+                    <p className="text-gray-700">{product.description}</p>
+                </div>
+            </div>
+
+            <div className="mt-8 border-t pt-4">
+                <a href="/products" className="text-blue-500 hover:underline">
+                    ← Back to Products
+                </a>
+            </div>
+        </div>
     )
 }
--- app/products/page.js
+++ app/products/page.js
@@ -1,15 +1,29 @@
 import Link from "next/link";
 
-export default function Page() {
+async function getProducts() {
+    const res = await fetch("https://dummyjson.com/products", {
+        next: { revalidate: 3600 }, // This works here!
+    });
+
+    if (!res.ok) throw new Error('Failed to fetch products');
+
+    const data = await res.json();
+    return data.products;
+}
+
+
+export default async function Page() {
+    const products = await getProducts();
+
     return (
         <div>
             <h1>Product page</h1>
             <h1>Select a product</h1>
             <ul>
-                {[...Array(10)].map((_, id) => (
-                    <li key={id}>
-                        <Link href={`/products/${id + 1}`}>
-                            Product {id}
+                {products.map((item, id) => (
+                    <li key={item.id}>
+                        <Link href={`/products/${item.id}`}>
+                            {item.title} -${item.price}
                         </Link>
                     </li>
                 ))}
